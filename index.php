0e64972f3162f2cda213556a33c7cf7aaa28eefc
ajikamaludin <aji19kamaludin@gmail.com>
2017-05-27 11:31:16 +0700

    Nambah Luas Persegi

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+function luas_persegi($a,$b){
+    return $a * $b;
+};
+
 
 echo luas_segitiga(2,3);
-echo luas_persegi();
+echo luas_persegi(4,5);
 echo keliling_persegi();
 echo diameter_lingkaran();
+
+?>
