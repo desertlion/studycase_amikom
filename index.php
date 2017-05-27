a98824e07f4bcb506c6de6a410939159433a7d93
ajikamaludin <aji19kamaludin@gmail.com>
2017-05-27 11:37:48 +0700

    revisi persegi

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-function luas_persegi($a,$b){
-    return $a * $b;
+function luas_persegi($a){
+    return $a * $a;
 };
 
 
 //echo luas_segitiga(2,3);
-echo luas_persegi(4,5);
+echo luas_persegi(4);
 //echo keliling_persegi();
 //echo diameter_lingkaran();
 
